acb468e409cbdcf081e8c4be28a4a72410364268
Nobir <nobirfreelancer@gmail.com>
2021-02-22 22:46:19 +0600

    added ShowInfo() method and override method

--- Inheritance/Inheritance/Account.cs
+++ Inheritance/Inheritance/Account.cs
@@ -8,9 +8,17 @@ namespace Inheritance
 {
     class Account
     {
-        string accName;
-        string accid;
-        int balance;
+        private string accName;
+        private string accid;
+        private int balance;
+        public Account() { }
+
+        public Account(string accName, string accid, int balance)
+        {
+            this.accName = accName;
+            this.accid = accid;
+            this.balance = balance;
+        }
 
         public string AccName
         {
@@ -30,31 +38,30 @@ namespace Inheritance
             get { return balance; }
         }
 
-        public Account() { }
-
-        public Account(string accName, string accid, int balance)
-        {
-            this.accName = accName;
-            this.accid = accid;
-            this.balance = balance;
-        }
-        public void Deposit(int amount)
+        public virtual void Deposit(int amount)
         {
             balance = balance + amount;
-            Console.WriteLine("Money Deposit Completed.");
-            Console.WriteLine();
         }
 
-        public void Withdraw(int amount)
+        public virtual void Withdraw(int amount)
         {
             balance = balance - amount;
-            Console.WriteLine("Money Withdraw Completed.");
         }
-        public void Transfer(int amount, Account acc)
+        public virtual void Transfer(int amount, Account acc)
         {
             balance = balance - amount;
             acc.Balance = acc.Balance + amount;
             Console.WriteLine("Balance Transfered.");
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Account Name: {0}", this.AccName);
+            Console.WriteLine("Account ID: {0}", this.Accid);
+            Console.WriteLine("Account Balance: {0}", this.Balance);
+            Console.WriteLine("Account Type: {0}", this.GetType().ToString().Split('.')[1]);
+            Console.WriteLine();
         }
     }
 }
